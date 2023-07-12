86af94d7108cd30e171e3091cabcd32721390e25
Carlo Wisse <contact@carlowisse.com>
2023-07-12 12:47:16 +1000

    deduplicated

--- scripts/python/de_duplicate.py
+++ scripts/python/de_duplicate.py
@@ -1,35 +1,38 @@
 import os
+from globals import domain_folder_path
+
+### VARS ###
+line_count_before = 0
+line_count_after = 0
+current_line = 0
+line_count_before = 0
+lines_processed = set()
 
 
-def process_file(file_path, line_count_before, line_count_after, current_line):
+def process_file(file_path, line_count_after, current_line):
     print(f"Processing file: {file_path}")
 
-    unique_lines = set()
-
+    lines = []
     with open(file_path, "r") as file:
         for line in file:
             line = line.strip()
-            if line not in unique_lines:
-                unique_lines.add(line)
+            if line not in lines_processed:
+                lines.append(line)
+                lines_processed.add(line)
+                line_count_after += 1
 
             # Update progress in real-time
             current_line += 1
-            print(f"Lines processed: {current_line}/{total_lines}", end="\r")
+            print(f"Lines processed: {current_line}/{line_count_before}", end="\r")
 
-    # Rewrite the file with unique lines
+    # Rewrite the file with unique lines in the original order
     with open(file_path, "w") as file:
-        file.writelines(f"{line}\n" for line in unique_lines)
+        file.write("\n".join(lines))
 
-    # Increment line count before deduplication
-    with open(file_path, "r") as file:
-        line_count_before += sum(1 for _ in file)
-
-    line_count_after += len(unique_lines)
-
-    return line_count_before, line_count_after, current_line
+    return line_count_after, current_line
 
 
-def process_folder(folder_path, line_count_before, line_count_after, current_line):
+def process_folder(folder_path, line_count_after, current_line):
     print(f"Processing folder: {folder_path}")
 
     entries = sorted(os.listdir(folder_path))
@@ -38,36 +41,24 @@ def process_folder(folder_path, line_count_before, line_count_after, current_lin
         entry_path = os.path.join(folder_path, entry)
 
         if os.path.isdir(entry_path):
-            line_count_before, line_count_after, current_line = process_folder(
-                entry_path, line_count_before, line_count_after, current_line
-            )
+            line_count_after, current_line = process_folder(entry_path, line_count_after, current_line)
         elif os.path.isfile(entry_path) and entry.endswith(".txt"):
-            line_count_before, line_count_after, current_line = process_file(
-                entry_path, line_count_before, line_count_after, current_line
-            )
+            line_count_after, current_line = process_file(entry_path, line_count_after, current_line)
 
-    return line_count_before, line_count_after, current_line
+    return line_count_after, current_line
 
-
-# Input folder
-input_folder = input("Enter the input folder: ")
-
-# Validate input folder
-if not os.path.isdir(input_folder):
-    print(f"Invalid input folder: {input_folder}")
-    exit(1)
-
-# Variables
-line_count_before = 0
-line_count_after = 0
-current_line = 0
-total_lines = 0
 
 # Calculate total lines
-line_count_before, line_count_after, current_line = process_folder(
-    input_folder, line_count_before, line_count_after, current_line
-)
-total_lines = current_line
+for root, _, files in os.walk(domain_folder_path):
+    for file in files:
+        if file.endswith(".txt"):
+            with open(os.path.join(root, file), "r") as f:
+                lines = [line.strip() for line in f]
+                unique_lines = set(lines)
+                line_count_before += len(unique_lines)
+
+# Process folders and files
+line_count_after, current_line = process_folder(domain_folder_path, line_count_after, current_line)
 
 # Clear progress line
 print()
